fad8c4e7e25613d79716c61e13241a0241fddc0f
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-03-21 22:24:27 +0530

    update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Project Inventory System.')
 });
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`))
