3a3cfb3b5816e339b58b37e9e0138f4fe312b1c5
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-03-22 02:04:25 +0530

    passportjs initialized

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
+// passport-local strategy
+// passport.use(new LocalStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`))
