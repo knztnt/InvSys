e60385ccde1a2fc120ab31d7c0d8108aa49e8d9a
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-03-23 19:01:14 +0530

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-const App = () => {
-    return (
-        <div>
-            Hi there!
-        </div>
-    );
-};
+import DemoNavbar from './DemoNavbar';
+import Login from '../views/Login';
+
+class App extends React.Component {
+    componentDidMount() {
+        document.documentElement.scrollTop = 0;
+        document.scrollingElement.scrollTop = 0;
+        this.refs.main.scrollTop = 0;
+    }
+    render() {
+        return (
+            <>
+                <main ref="main">
+                    <DemoNavbar />
+                    <Login />
+
+                </main>
+            </>
+        );
+    }
+}
 
 export default App;
