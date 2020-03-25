254c4cd6e83dfe8c155fd79e3158b48bb226191c
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-03-25 09:07:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const Users = require("./routes/authRoutes");
+
+app.use("/users", Users);
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
