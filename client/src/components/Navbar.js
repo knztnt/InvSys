a517fa9b13951d489378a1dfbc00d1d07f82ac90
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-03-25 10:57:03 +0530

    navar update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
 
 class Landing extends Component {
   logOut(e) {
-    e.preventDefault()
-    localStorage.removeItem('usertoken')
-    this.props.history.push(`/`)
+    e.preventDefault();
+    localStorage.removeItem("usertoken");
+    this.props.history.push(`/`);
   }
 
   render() {
@@ -22,7 +22,7 @@ class Landing extends Component {
           </Link>
         </li>
       </ul>
-    )
+    );
 
     const userLink = (
       <ul className="navbar-nav">
@@ -37,11 +37,13 @@ class Landing extends Component {
           </a>
         </li>
       </ul>
-    )
+    );
 
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href="#/">Test</a>
+        <a className="navbar-brand" href="#/">
+          InvSys
+        </a>
         <button
           className="navbar-toggler"
           type="button"
@@ -54,23 +56,30 @@ class Landing extends Component {
           <span className="navbar-toggler-icon" />
         </button>
 
-        <div
-          className="collapse navbar-collapse justify-content-md-center"
-          id="navbarsExample10"
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item">
+        <div className="collapse navbar-collapse" id="navbarsExample10">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
               <Link to="/" className="nav-link">
                 Home
+                <span class="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Contact
               </Link>
             </li>
           </ul>
           {localStorage.usertoken ? userLink : loginRegLink}
         </div>
       </nav>
-
-    )
+    );
   }
 }
 
-export default withRouter(Landing)
+export default withRouter(Landing);
