769a0ee2af134a73fe865e34d06b4d0380683334
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-03-25 07:54:34 +0530

    remove all

--- client/src/reducers/authReducer.js
+++
@@ -1,7 +0,0 @@
-export default function (state = {}, action) {
-    switch (action.type) {
-        default:
-            return state;
-    }
-
-}
--- client/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function (app) {
-    app.use(
-        createProxyMiddleware(
-            ['/auth/register', '/auth/login'], { target: 'http://localhost:5000' }
-        ));
-}
