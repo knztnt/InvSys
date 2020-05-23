e9a120ce623f724e1bb90cbd59ec0f724e690af4
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-05-23 16:28:39 +0530

    minor updates

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
+// port 5000 for the server
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
