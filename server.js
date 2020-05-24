3190497d45bb247b8f293358e0d9b692c9feca0f
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-05-24 18:13:30 +0530

    initalize sequelize

--- server.js
+++ server.js
@@ -17,14 +17,48 @@ app.use(bodyParser.json());
 // parse requests of content type x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// database sync
+const db = require("./app/models");
+const Role = db.role;
+db.sequelize.sync();
+/*
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+*/
+
 // test route
 app.get("/", (req, res) => {
     res.json({ message: "InvSys Server Running..." });
 });
 
-const Users = require("./routes/authRoutes");
-app.use("/users", Users);
+// const Users = require("./routes/authRoutes");
+// app.use("/users", Users);
 
 // port 5000 for the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
+
+// create roles in database
+function initial() {
+    Role.create({
+        id: 1,
+        name: "Admin"
+    });
+
+    Role.create({
+        id: 2,
+        name: "Non-Academic"
+    });
+
+    Role.create({
+        id: 3,
+        name: "Academic"
+    });
+
+    Role.create({
+        id: 4,
+        name: "Student"
+    });
+}
