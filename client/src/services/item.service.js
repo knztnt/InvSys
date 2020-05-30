f237b43c6b471393075ec34358df0002f1a0ca3f
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-05-30 15:34:50 +0530

    Create item.service.js

---
+++ client/src/services/item.service.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/item/';
+
+class ItemService {
+    create(data) {
+        return axios.post(API_URL + "create", { data });
+    }
+
+    getall() {
+        return axios.get(API_URL + "getall");
+    }
+
+    getItem(item_no) {
+        return axios.get(API_URL + ${ item_no });
+    }
+
+    update(item_no, data) {
+        return axios.put(API_URL + "update" + ${ item_no }, { data });
+    }
+}
+
+export default new ItemService();
