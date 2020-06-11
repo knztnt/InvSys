9e06367d5dbe8c97951ce6d3889ebc7be478e4ff
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-11 20:20:08 +0530

    get all users backend
    Co-Authored-By: Anandi Karunaratne <anandikarunaratne@users.noreply.github.com>

--- app/controllers/user-role.controller.js
+++ app/controllers/user-role.controller.js
@@ -22,3 +22,63 @@ exports.getAllAcademic = (req, res) => {
             });
         });
 };
+
+// get all non-academic users
+exports.getAllNonAcademic = (req, res) => {
+    userRole.findAll({
+        where: {
+            roleId: {
+                [Op.eq]: 2
+            }
+        }
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving items."
+            });
+        });
+};
+
+// get all student users
+exports.getAllStudent = (req, res) => {
+    userRole.findAll({
+        where: {
+            roleId: {
+                [Op.eq]: 4
+            }
+        }
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving items."
+            });
+        });
+};
+
+// get all admin users
+exports.getAllAdmin = (req, res) => {
+    userRole.findAll({
+        where: {
+            roleId: {
+                [Op.eq]: 1
+            }
+        }
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving items."
+            });
+        });
+};
--- app/routes/user-role.routes.js
+++ app/routes/user-role.routes.js
@@ -3,4 +3,13 @@ const controller = require("../controllers/user-role.controller");
 module.exports = function (app) {
     // get all academic users
     app.get("/api/users/academic", controller.getAllAcademic);
+
+    // get all non-academic users
+    app.get("/api/users/non-academic", controller.getAllNonAcademic);
+
+    // get all academic users
+    app.get("/api/users/student", controller.getAllStudent);
+
+    // get all academic users
+    app.get("/api/users/admin", controller.getAllAdmin);
 };
