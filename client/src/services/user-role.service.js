c047a1d90083224a2780c3297bddaeaec9983ff5
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-11 20:27:42 +0530

    Update user-role.service.js

--- client/src/services/user-role.service.js
+++ client/src/services/user-role.service.js
@@ -6,6 +6,18 @@ class UserRoles {
     getallAcademic() {
         return axios.get(API_URL + "academic");
     }
+
+    getallNonAcademic() {
+        return axios.get(API_URL + "non-academic");
+    }
+
+    getallStudent() {
+        return axios.get(API_URL + "student");
+    }
+
+    getallAdmin() {
+        return axios.get(API_URL + "admin");
+    }
 }
 
 export default new UserRoles();
