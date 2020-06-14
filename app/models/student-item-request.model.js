03ff8f3b62f205badc00196ab274e3649da96442
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-14 23:55:28 +0530

    Update student-item-request.model.js
    Co-Authored-By: Anandi Karunaratne <anandikarunaratne@users.noreply.github.com>

--- app/models/student-item-request.model.js
+++ app/models/student-item-request.model.js
@@ -3,36 +3,35 @@ module.exports = (sequelize, Sequelize) => {
     const Student_Item_Request = sequelize.define(
         "student_item_requests",
         {
+            requestId: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
             studentId: {
                 type: Sequelize.STRING
             },
             item_no: {
                 type: Sequelize.STRING
             },
-            item_name: {
+            staffId: {
                 type: Sequelize.STRING
             },
             quantity: {
                 type: Sequelize.INTEGER
             },
-            description: {
-                type: Sequelize.STRING(1000)
-            },
-            staffId: {
-                type: Sequelize.STRING
-            },
             reason: {
                 type: Sequelize.STRING(1000)
-            },
-            isApproved: {
-                type: Sequelize.BOOLEAN,
-            },
-            proceededTime: {
-                type: Sequelize.DATE
-            },
-            reviewedTime: {
-                type: Sequelize.DATE
             }
+            // isApproved: {
+            //     type: Sequelize.BOOLEAN,
+            // },
+            // proceededTime: {
+            //     type: Sequelize.DATE
+            // },
+            // reviewedTime: {
+            //     type: Sequelize.DATE
+            // }
         },
         {
             timestamps: true,
