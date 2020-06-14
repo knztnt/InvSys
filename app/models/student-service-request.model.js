0c6b1dfd70d779cf44a8a3a27c53f3f090cb614b
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-14 23:55:31 +0530

    Update student-service-request.model.js
    Co-Authored-By: Anandi Karunaratne <anandikarunaratne@users.noreply.github.com>

--- app/models/student-service-request.model.js
+++ app/models/student-service-request.model.js
@@ -3,33 +3,32 @@ module.exports = (sequelize, Sequelize) => {
     const Student_Service_Request = sequelize.define(
         "student_service_requests",
         {
+            requestId: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
             studentId: {
                 type: Sequelize.STRING
             },
             service_no: {
                 type: Sequelize.STRING
             },
-            service_name: {
-                type: Sequelize.STRING
-            },
-            description: {
-                type: Sequelize.STRING(1000)
-            },
             staffId: {
                 type: Sequelize.STRING
             },
             reason: {
                 type: Sequelize.STRING(1000)
-            },
-            isApproved: {
-                type: Sequelize.BOOLEAN,
-            },
-            proceededTime: {
-                type: Sequelize.DATE
-            },
-            reviewedTime: {
-                type: Sequelize.DATE
             }
+            // isApproved: {
+            //     type: Sequelize.BOOLEAN,
+            // },
+            // proceededTime: {
+            //     type: Sequelize.DATE
+            // },
+            // reviewedTime: {
+            //     type: Sequelize.DATE
+            // }
         },
         {
             timestamps: true,
