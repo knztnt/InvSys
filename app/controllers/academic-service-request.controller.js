229dfca0efe93cf0ec742ebd0cb39df6b3198887
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-15 14:45:46 +0530

    Update academic-service-request.controller.js

--- app/controllers/academic-service-request.controller.js
+++ app/controllers/academic-service-request.controller.js
@@ -17,8 +17,6 @@ exports.create = (req, res) => {
     const request = {
         academicId: req.body.academicId,
         service_no: req.body.service_no,
-        service_name: req.body.service_name,
-        description: req.body.description,
         reason: req.body.reason
     };
 
