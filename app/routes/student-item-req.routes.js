cc3acc4c78bf528e19be028532b64ffeba39c84b
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-15 17:31:20 +0530

    Update student-item-req.routes.js

--- app/routes/student-item-req.routes.js
+++ app/routes/student-item-req.routes.js
@@ -3,4 +3,17 @@ const controller = require("../controllers/student-item-request.controller");
 module.exports = function (app) {
     // Create a new request
     app.post("/api/item/req/create", controller.create);
+
+    // Retrieve all Items
+    app.get("/api/item/req/getall", controller.findAll);
+
+    // Retrieve all published Items
+    app.get("/api/item/req/reviewed", controller.findAllReviewed);
+
+    // Retrieve a single Item with requestId
+    app.get("/api/item/req/:requestId", controller.findOne);
+
+    // Update a Item with requestId
+    app.put("/api/item/req/update/:requestId", controller.update);
+
 };
