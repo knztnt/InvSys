ca0e45acd286548a02638197baac9c3e798852fe
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-15 17:32:08 +0530

    Create reviewed-item-req.controller.js

---
+++ app/controllers/reviewed-item-req.controller.js
@@ -0,0 +1,35 @@
+const db = require("../models");
+const ReviewedItemReq = db.reviewed_item_request;
+
+const Op = db.Sequelize.Op;
+
+// Create and Save a new request
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.requestId) {
+        res.status(400).send({
+            message: "Content cannot be empty!"
+        });
+        return;
+    }
+
+    // Create a Request
+    const request = {
+        requestId: req.body.requestId,
+        remarks: req.body.remarks,
+        isApproved: req.body.isApproved,
+        reviewedTime: req.body.reviewedTime
+    };
+
+    // Save Request in the database
+    ReviewedItemReq.create(request)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Request."
+            });
+        });
+};
