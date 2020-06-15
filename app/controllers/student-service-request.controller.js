0f3bf8afe4bfddc6edccfad7adbbb8d3c4ff9fb7
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-15 17:31:56 +0530

    Update student-service-request.controller.js

--- app/controllers/student-service-request.controller.js
+++ app/controllers/student-service-request.controller.js
@@ -33,3 +33,74 @@ exports.create = (req, res) => {
             });
         });
 };
+
+// Retrieve all Requests from the database
+exports.findAll = (req, res) => {
+    const requestId = req.query.requestId;
+    var condition = requestId ? { requestId: { [Op.like]: `%${requestId}%` } } : null;
+
+    StudServiceReq.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving items."
+            });
+        });
+};
+
+// Find a single Request with an requestId
+exports.findOne = (req, res) => {
+    const requestId = req.params.requestId;
+
+    StudServiceReq.findByPk(requestId)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Request with item number = " + requestId
+            });
+        });
+};
+
+// find all reviewed requests
+exports.findAllReviewed = (req, res) => {
+    StudServiceReq.findAll({ where: { isReviewed: true } })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving items."
+            });
+        });
+};
+
+// Update a Request by the requestId in the request
+exports.update = (req, res) => {
+    const requestId = req.params.requestId;
+
+    StudServiceReq.update(req.body, {
+        where: { requestId: requestId }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Request was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Request with requestId=${requestId}. Maybe Request was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Request with requestId=" + requestId
+            });
+        });
+};
