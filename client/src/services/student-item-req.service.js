e27f9d0ff535375364461305c4911ae5a3c04c86
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-16 15:46:07 +0530

    minor changes

--- client/src/services/student-item-req.service.js
+++ client/src/services/student-item-req.service.js
@@ -3,13 +3,11 @@ import axios from "axios";
 const API_URL = "http://localhost:5000/api/item/req/";
 
 class StudItemReqService {
-    create(studentId, item_no, item_name, quantity, description, staffId, reason) {
+    create(studentId, item_no, quantity, staffId, reason) {
         return axios.post(API_URL + "create", {
             studentId,
             item_no,
-            item_name,
             quantity,
-            description,
             staffId,
             reason
         });
--- client/src/services/student-service-req.service.js
+++ client/src/services/student-service-req.service.js
@@ -3,12 +3,10 @@ import axios from "axios";
 const API_URL = "http://localhost:5000/api/service/req/";
 
 class StudServiceReqService {
-    create(studentId, service_no, service_name, description, staffId, reason) {
+    create(studentId, service_no, staffId, reason) {
         return axios.post(API_URL + "create", {
             studentId,
             service_no,
-            service_name,
-            description,
             staffId,
             reason
         });
