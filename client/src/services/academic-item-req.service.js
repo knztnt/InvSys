17c6c01ec3038c37961cac7b7d38ba3cb4be0fa9
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-16 15:45:53 +0530

    minor changes

--- client/src/services/academic-item-req.service.js
+++ client/src/services/academic-item-req.service.js
@@ -3,13 +3,11 @@ import axios from "axios";
 const API_URL = "http://localhost:5000/api/item/ac/req/";
 
 class AcaItemReqService {
-    create(academicId, item_no, item_name, quantity, description, reason) {
+    create(academicId, item_no, quantity, reason) {
         return axios.post(API_URL + "create", {
             academicId,
             item_no,
-            item_name,
             quantity,
-            description,
             reason
         });
     }
