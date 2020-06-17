d21f82f7db3d2154cc5151c2334d59d9efc6a95a
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-17 13:34:27 +0530

    Update profile.controller.js

--- app/controllers/profile.controller.js
+++ app/controllers/profile.controller.js
@@ -6,8 +6,8 @@ const Op = db.Sequelize.Op;
 
 // Retrieve all users
 exports.findAll = (req, res) => {
-    const username = req.query.username;
-    var condition = username ? { username: { [Op.like]: `%${username}%` } } : null;
+    const first_name = req.query.first_name;
+    var condition = first_name ? { first_name: { [Op.like]: `%${first_name}%` } } : null;
 
     Profile.findAll({ where: condition })
         .then(data => {
@@ -21,56 +21,48 @@ exports.findAll = (req, res) => {
         });
 };
 
-// // Find a single Item with an item_no
-// exports.findOne = (req, res) => {
-//     const item_no = req.params.item_no;
+// Find a single profile with a username
+exports.findOne = (req, res) => {
+    const username = req.params.username;
 
-//     Item.findByPk(item_no)
-//         .then(data => {
-//             res.send(data);
-//         })
-//         .catch(err => {
-//             res.status(500).send({
-//                 message: "Error retrieving Item with item number = " + item_no
-//             });
-//         });
-// };
+    Profile.findByPk(username)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Profile with username = " + username
+            });
+        });
+};
 
-// const t = db.sequelize.transaction();
 
-// // Update a Item by the item_no in the request
-// exports.update = (req, res) => {
-//     const item_no = req.params.item_no;
+// Update a Profile by the username in the request
+exports.update = (req, res) => {
+    const username = req.params.username;
 
-//     try {
-//         Item.update(req.body.data, {
-//             where: { item_no: item_no }
-//         }, { transaction: t })
-//             .then(num => {
-//                 if (num == 1) {
-//                     res.send({
-//                         message: "Item was updated successfully."
-//                     });
-//                 } else {
-//                     res.send({
-//                         message: `Cannot update Item with item_no=${item_no}. Maybe Item was not found or req.body is empty!`
-//                     });
-//                 }
-//                 // commit the transaction.
-//                 t.commit();
-//             })
-//             .catch(err => {
-//                 res.status(500).send({
-//                     message: "Error updating Item with item_no=" + item_no
-//                 });
-//             });
-//     } catch (error) {
-//         // If the execution reaches this line, an error was thrown.
-//         // rollback the transaction.
-//         t.rollback();
 
-//     }
-// };
+    Profile.update(req.body.data, {
+        where: { username: username }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Profile was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Profile with username=${username}. Maybe Profile was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Profile with username=" + username
+            });
+        });
+
+};
 
 // // Delete a Item with the specified item_no in the request
 // exports.delete = (req, res) => {
