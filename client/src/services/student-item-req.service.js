d97187565e56c667635ad88b96259aa0cd8e1d2f
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-17 23:00:36 +0530

    new req services added

--- client/src/services/student-item-req.service.js
+++ client/src/services/student-item-req.service.js
@@ -12,6 +12,27 @@ class StudItemReqService {
             reason
         });
     }
+
+    getall() {
+        return axios.get(API_URL + "getall");
+    }
+
+    get(requestId) {
+        return axios.get(API_URL + requestId);
+    }
+
+    update(requestId, data) {
+        console.log(data);
+        return axios.put(API_URL + "update/" + requestId, { data });
+    }
+
+    findByStudId(studentId) {
+        return axios.get(API_URL + "getall/student?studentId=" + studentId);
+    }
+
+    findByAcId(staffId) {
+        return axios.get(API_URL + "getall/academic?staffId=" + staffId);
+    }
 }
 
 export default new StudItemReqService();
--- client/src/services/student-service-req.service.js
+++ client/src/services/student-service-req.service.js
@@ -11,6 +11,27 @@ class StudServiceReqService {
             reason
         });
     }
+
+    getall() {
+        return axios.get(API_URL + "getall");
+    }
+
+    get(requestId) {
+        return axios.get(API_URL + requestId);
+    }
+
+    update(requestId, data) {
+        console.log(data);
+        return axios.put(API_URL + "update/" + requestId, { data });
+    }
+
+    findByStudId(studentId) {
+        return axios.get(API_URL + "getall/student?studentId=" + studentId);
+    }
+
+    findByAcId(staffId) {
+        return axios.get(API_URL + "getall/academic?staffId=" + staffId);
+    }
 }
 
 export default new StudServiceReqService();
