17f37a188090caea9786248815e155f7ff04a849
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-06-19 12:51:19 +0530

    Update approve-service-req.component.js

--- client/src/components/request/approve-service-req.component.js
+++ client/src/components/request/approve-service-req.component.js
@@ -8,7 +8,8 @@ export default class ApproveServiceReq extends Component {
     constructor(props) {
         super(props);
         this.getRequests = this.getRequests.bind(this);
-        this.renderTableData = this.renderTableData.bind(this);
+        this.renderTablePending = this.renderTablePending.bind(this);
+        this.renderTableReviewed = this.renderTableReviewed.bind(this);
 
         this.state = {
             studentReq: [],
@@ -36,17 +37,36 @@ export default class ApproveServiceReq extends Component {
             });
     }
 
-    renderTableData() {
+    renderTablePending() {
         return this.state.studentReq.map((request, index) => {
-            const { requestId, studentId, service_no, reason } = request //destructuring
-            return (
-                <tr key={requestId}>
-                    <td>{studentId}</td>
-                    <td>{service_no}</td>
-                    <td>{reason}</td>
-                    <td>{<Link to={"/approve-service-requests/" + requestId}>Review Request</Link>}</td>
-                </tr>
-            )
+            if (!request.isReviewed) {
+                const { requestId, studentId, service_no, reason } = request //destructuring
+                return (
+                    <tr key={requestId}>
+                        <td>{studentId}</td>
+                        <td>{service_no}</td>
+                        <td>{reason}</td>
+                        <td>{<Link to={"/approve-service-requests/" + requestId}>Review Request</Link>}</td>
+                    </tr>
+                )
+            }
+            else { return null; }
+        })
+    }
+
+    renderTableReviewed() {
+        return this.state.studentReq.map((request, index) => {
+            if (request.isReviewed) {
+                const { requestId, studentId, service_no, reason } = request //destructuring
+                return (
+                    <tr key={requestId}>
+                        <td>{studentId}</td>
+                        <td>{service_no}</td>
+                        <td>{reason}</td>
+                    </tr >
+                )
+            }
+            else { return null; }
         })
     }
 
@@ -56,19 +76,47 @@ export default class ApproveServiceReq extends Component {
             <div className="container">
                 <h4>Service Requests</h4>
                 <hr />
-                <table className="table">
-                    <thead className="thead-light">
-                        <tr>
-                            <th scope="col">Student</th>
-                            <th scope="col">Service</th>
-                            <th scope="col">Reason</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderTableData()}
-                    </tbody>
-                </table>
+                <ul className="nav nav-pills mb-3" id="myTab" role="tablist">
+                    <li className="nav-item">
+                        <a className="nav-link active" id="pending-tab" data-toggle="tab" href="#pending" role="tab" aria-controls="pending" aria-selected="true">
+                            Pending Requests <span className="badge badge-light">{this.state.pending}</span>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" id="reviewed-tab" data-toggle="tab" href="#reviewed" role="tab" aria-controls="reviewed" aria-selected="false">Reviewed Requests</a>
+                    </li>
+                </ul>
+                <div className="tab-content" id="myTabContent">
+                    <div className="tab-pane fade show active" id="pending" role="tabpanel" aria-labelledby="pending-tab">
+                        <table className="table">
+                            <thead className="thead-light">
+                                <tr>
+                                    <th scope="col">Student</th>
+                                    <th scope="col">Service</th>
+                                    <th scope="col">Reason</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.renderTablePending()}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="tab-pane fade" id="reviewed" role="tabpanel" aria-labelledby="reviewed-tab">
+                        <table className="table">
+                            <thead className="thead-light">
+                                <tr>
+                                    <th scope="col">Student</th>
+                                    <th scope="col">Service</th>
+                                    <th scope="col">Reason</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.renderTableReviewed()}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         );
     }
